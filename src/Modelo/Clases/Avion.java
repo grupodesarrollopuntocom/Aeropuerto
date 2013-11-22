f9c76a961387c81189f210d0bf03de9d643d1b3b
gabriel <gabriel@192.168.1.128>
2013-11-22 21:41:51 +0100

    Realizando prubas de GituHub sin importancia

--- src/Modelo/Clases/Avion.java
+++ src/Modelo/Clases/Avion.java
@@ -7,6 +7,9 @@ public class Avion {
     private int peso;
     private double dimensionLargo;
     private double dimensionAncho;
+    private int cont;
+    private String prueba;
+    
 
     public Avion(int codigo, String Companya, int capacidad, int peso, double dimensionLargo, double dimensionAncho) {
         this.codigo = codigo;
